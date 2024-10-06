9acc8c4dbef127c1309f05a58a594f8a2945034f
Monzurul Islam <monzurul.ce.buet@gmail.com>
2024-10-06 18:26:17 -0500

    task: add footer

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -1,48 +1,41 @@
-// import { Box ,Flex,Icon,Link,Text,  useColorModeValue,} from "@chakra-ui/react";
-// import { FaGithub, FaLinkedin, FaTwitter, FaResearchgate } from "react-icons/fa";
-// import { SiGooglescholar } from "react-icons/si";
-// import socialMediaData from "../Datas/socialMediaData.json";
+/** @format */
 
+import { Box, Text, Link, Stack, Icon } from "@chakra-ui/react";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
+export default function Footer() {
+  return (
+    <Box as="footer" bg="gray.700" color="white" py={6}>
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        justify="space-between"
+        align="center"
+        maxW="container.xl"
+        mx="auto"
+        px={4}
+      >
+        {/* Footer Links with Icons */}
+        <Stack direction="row" spacing={6}>
+          <Link href="mailto:subasish@txstate.edu" isExternal fontSize="2xl">
+            <Icon as={FaEnvelope} mr={2} color="white" />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/subasishdas/"
+            isExternal
+            fontSize="2xl"
+          >
+            <Icon as={FaLinkedin} mr={2} color="white" />
+          </Link>
+          <Link href="https://github.com/subasish" isExternal fontSize="2xl">
+            <Icon as={FaGithub} mr={2} color="white" />
+          </Link>
+        </Stack>
 
-
-// export default function Footer() {
-
-//   const backgroundColor = useColorModeValue('#0a1e30', '#0a1e30');
-
-//   const iconMap = {
-//     FaGithub: FaGithub,
-//     FaLinkedin: FaLinkedin,
-//     FaTwitter: FaTwitter,
-//     FaResearchgate: FaResearchgate,
-//     SiGooglescholar: SiGooglescholar
-//   };
-
-
-//   return (
-//     <Box bg={backgroundColor} px={{base:"1rem",md:"1rem",lg:"4rem",xl:"4rem"}} pb="1rem">
-//         <Flex direction={{base:"column",md:"column",lg:"row",xl:"row"}} justifyContent="space-between" alignItems="center" height="6rem">
-//           <Box height="4rem" alignContent="center">
-//             {socialMediaData.links.map((link, index) => {
-//               const IconComponent = iconMap[link.icon];
-//               return (
-//                 <Link href={link.href} isExternal key={index}>
-//                   <Icon boxSize="2.3rem" as={IconComponent} color={link.color} pl="0.8rem" />
-//                 </Link>
-//               );
-//             })}
-//           </Box>
-//           <Box >
-//             <Text fontSize="sm" fontWeight="200" textAlign="center">Copyright @ {new Date().getFullYear()} Subasish Das. All rights reserved.</Text>
-//           </Box>
-//         </Flex>
-//     </Box>
-//   );
-// }
-
-
-
-export default function Footer(){
-  <>
-  </>
+        {/* Copyright Information */}
+        <Text textAlign={{ base: "center", md: "right" }}>
+          &copy; {new Date().getFullYear()} AIT Lab. All rights reserved.
+        </Text>
+      </Stack>
+    </Box>
+  );
 }
