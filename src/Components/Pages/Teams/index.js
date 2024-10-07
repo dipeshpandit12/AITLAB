52de896cbe514898180ee4135c750aceb3238d33
Monzurul Islam <monzurul.ce.buet@gmail.com>
2024-10-07 00:43:20 -0500

    wip: images added

--- src/Components/Pages/Teams/index.js
+++ src/Components/Pages/Teams/index.js
@@ -1,82 +1,124 @@
-import { Box, Flex, Heading, Button, HStack } from "@chakra-ui/react";
-import ProfileCard from "./ProfileCard";
-import Teams_Datas from "../../../Datas/Teams_Data.json";
-import Modal_Page from "./Modal_Page";
-import { useState } from "react";
+/** @format */
 
-export default function Teams() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedAlumniData, setSelectedAlumniData] = useState(null);
+import React from "react";
+import {
+  Box,
+  Container,
+  Text,
+  SimpleGrid,
+  Image,
+  Link,
+  Stack,
+  Button,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
+import { FaGithub, FaLinkedin, FaGoogle } from "react-icons/fa";
+import teamData from "../../../data/team.json"; // Assume the JSON file is located here
+
+const Team = () => {
+  // Group the team members by their "group" attribute
+  const groupedTeamMembers = teamData.reduce((acc, member) => {
+    if (!acc[member.group]) {
+      acc[member.group] = [];
+    }
+    acc[member.group].push(member);
+    return acc;
+  }, {});
 
   return (
-    <Box px={{ lg: "5rem", xl: "5rem" }} py={{ lg: "3rem", xl: "3rem" }}>
-      {/* ------------------------For Current Members------------------------------ */}
-      <Flex direction={{ base: "column", md: "column", lg: "row", xl: "row" }}>
-        <Box
-          width={{ base: "100%", md: "30%", lg: "30%", xl: "30%" }}
-          pt="3rem"
-        >
-          <Heading size="xl" pl="1rem">
-            Current Team
-          </Heading>
-        </Box>
-        <Box width={{ base: "100%", md: "70%", lg: "70%", xl: "70%" }}>
-          {Teams_Datas.filter((data) => data.status === "current").map(
-            (data, index) => (
-              <ProfileCard key={index} data={data} />
-            )
-          )}
-        </Box>
-      </Flex>
+    <Box py={8}>
+      <Container maxW="container.xl">
+        <Heading as="h1" size="2xl" mb={6} color="teal.600">
+          Current Team
+        </Heading>
 
-      {/* ------------------------For Alumni------------------------------ */}
-      <Flex direction={{ base: "column", md: "column", lg: "row", xl: "row" }}>
-        <Box
-          width={{ base: "100%", md: "30%", lg: "30%", xl: "30%" }}
-          pt="3rem"
-        >
-          <Heading size="xl" pl="1rem" pb="3rem">
-            Alumni
-          </Heading>
-        </Box>
-        <Box
-          width={{ base: "100%", md: "70%", lg: "70%", xl: "70%" }}
-          p={{ base: "1rem", lg: "8rem" }}
-        >
-          {Teams_Datas.filter((data) => data.status === "Alumni").map(
-            (data, index) => (
-              <HStack spacing={5} p="0.7rem">
-                <Heading fontSize="lg" key={index}>
-                  {data.name}
-                </Heading>
-                <Button
-                  onClick={() => {
-                    setSelectedAlumniData(data); // Set the selected alumni data
-                    setIsModalOpen(true);
-                  }} // Set state to open the modal
-                  variant="link"
-                  size="sm"
-                  color="teal"
-                  pt="1rem"
+        {/* Iterate over each group and display the team members */}
+        {Object.keys(groupedTeamMembers).map((group) => (
+          <Box key={group} mb={10}>
+            <Text fontSize="2xl" fontWeight="bold" mb={4} color="gray.700">
+              {group}
+            </Text>
+            <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
+              {groupedTeamMembers[group].map((member, index) => (
+                <Box
+                  key={index}
+                  p={5}
+                  shadow="md"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  bg="white"
                 >
-                  Know More
-                </Button>
-              </HStack>
-            )
-          )}
-          {/* --------------------------For Modal -----------------------------------------*/}
-
-          {/* Modal for Full Details of Alumni */}
-          {isModalOpen && (
-            // eslint-disable-next-line react/jsx-pascal-case
-            <Modal_Page
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-              data={selectedAlumniData}
-            />
-          )}
-        </Box>
-      </Flex>
+                  <Flex direction={{ base: "column", md: "row" }}>
+                    <Image
+                      src={member.image || "https://via.placeholder.com/150"}
+                      alt={member.name}
+                      boxSize="150px"
+                      objectFit="cover"
+                      borderRadius="full"
+                      mr={{ md: 6 }}
+                      mb={{ base: 4, md: 0 }}
+                    />
+                    <Stack spacing={3} flex="1">
+                      <Text fontSize="lg" fontWeight="bold" color="teal.800">
+                        {member.name}
+                      </Text>
+                      <Text fontSize="sm" color="gray.500">
+                        {member.label}
+                      </Text>
+                      <Text fontSize="sm" color="gray.600" noOfLines={3}>
+                        {member.description}
+                      </Text>
+                      <Box mt={3}>
+                        {member.github && (
+                          <Link
+                            href={member.github}
+                            isExternal
+                            mr={3}
+                            color="gray.700"
+                          >
+                            <FaGithub size="20px" />
+                          </Link>
+                        )}
+                        {member.linkedin && (
+                          <Link
+                            href={member.linkedin}
+                            isExternal
+                            mr={3}
+                            color="gray.700"
+                          >
+                            <FaLinkedin size="20px" />
+                          </Link>
+                        )}
+                        {member.googleScholar && (
+                          <Link
+                            href={member.googleScholar}
+                            isExternal
+                            mr={3}
+                            color="gray.700"
+                          >
+                            <FaGoogle size="20px" />
+                          </Link>
+                        )}
+                      </Box>
+                      <Button
+                        mt={2}
+                        variant="link"
+                        color="teal.500"
+                        _hover={{ textDecoration: "underline" }}
+                      >
+                        Show more
+                      </Button>
+                    </Stack>
+                  </Flex>
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Box>
+        ))}
+      </Container>
     </Box>
   );
-}
+};
+
+export default Team;
